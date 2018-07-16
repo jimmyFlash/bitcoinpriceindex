2b2d59fe9e3072fdaffae5f19865cb1a825657a9
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2018-07-16 20:48:18 +0200

    - create utilities class in kotlin

---
+++ app/src/main/java/com/jimmy/bitcoinpriceindex/utils/UtilsKotlin.kt
@@ -0,0 +1,69 @@
+package com.jimmy.bitcoinpriceindex.utils
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.jimmy.bitcoinpriceindex.Constants
+import com.jimmy.bitcoinpriceindex.R
+
+
+object UIUtils {
+
+     val TAG = UIUtils::class.java.simpleName
+
+
+    /**
+     * Method for sleeping for a fixed about of time to emulate slower work
+     */
+    fun sleep() {
+        try {
+            Thread.sleep(Constants.DELAY_TIME_MILLIS, 0)
+        } catch (e: InterruptedException) {
+            Log.d(TAG, e.message)
+        }
+    }
+
+    /**
+     * Create a Notification that is shown as a heads-up notification if possible.
+     *
+     * For this codelab, this is used to show a notification so that you know when different steps
+     * of the background work chain are starting
+     *
+     * @param message Message shown on the notification
+     * @param context Context needed to create Toast
+     */
+    fun makeStatusNotification(message: String, context: Context) {
+
+        // Make a channel if necessary
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            val name = Constants.VERBOSE_NOTIFICATION_CHANNEL_NAME
+            val description = Constants.VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(Constants.CHANNEL_ID, name, importance)
+            channel.description = description
+
+            // Add the channel
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // Create the notification
+        val builder = NotificationCompat.Builder(context, Constants.CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(Constants.NOTIFICATION_TITLE)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(LongArray(0))
+
+        // Show the notification
+        NotificationManagerCompat.from(context).notify(Constants.NOTIFICATION_ID, builder.build())
+    }
+
+}
