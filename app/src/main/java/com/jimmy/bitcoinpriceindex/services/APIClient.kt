0fe4dfc0bd64029ceb117a39006743219516457b
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2018-08-03 11:57:16 +0200

    - add trust certificate override method for accepting any certificate, useful when behind firewall or proxy, overrides OkHTTP secure certificate SSL exception

--- app/src/main/java/com/jimmy/bitcoinpriceindex/services/APIClient.kt
+++ app/src/main/java/com/jimmy/bitcoinpriceindex/services/APIClient.kt
@@ -5,6 +5,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import javax.net.ssl.*
 
 /**
@@ -23,9 +24,13 @@ class APIClient {
             val interceptor = HttpLoggingInterceptor()
             interceptor.level = HttpLoggingInterceptor.Level.BODY
 
-            val client = OkHttpClient.Builder()
+       /*     val client = OkHttpClient.Builder()
                     .addInterceptor(interceptor)
-                    .build()
+                    .build()*/
+
+            val client = getUnsafeOkHttpClient()
+                    ?.addInterceptor(interceptor)
+                    ?.build()
 
             retrofit = Retrofit.Builder()
                     .baseUrl("https://api.coindesk.com/v1/")
@@ -34,6 +39,46 @@ class APIClient {
                     .build()
 
             return retrofit!!
+
+        }
+
+        /**
+         * enables okhttp to accept untrusted certificates (when behind firewall or proxy)
+         */
+        fun getUnsafeOkHttpClient() : OkHttpClient.Builder?{
+
+            try {
+
+                val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager{
+                    override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+                       //
+                    }
+
+                    override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+                      //
+                    }
+                    override fun getAcceptedIssuers(): Array<X509Certificate> {
+                        return arrayOf()
+                    }
+
+                })
+                // Create a trust manager that does not validate certificate chains
+                // Install the all-trusting trust manager
+                val sslContext = SSLContext.getInstance("SSL")
+                sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+                // Create an ssl socket factory with our all-trusting manager
+                val  sslSocketFactory : SSLSocketFactory = sslContext.socketFactory
+
+                val builder = OkHttpClient.Builder()
+                builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+
+                builder.hostnameVerifier { hostname, session -> true }
+                return builder
+            } catch ( e : Exception) {
+//                throw  RuntimeException(e)
+                return null
+            }
 
         }
     }
